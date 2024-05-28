242a6c3e2fa2e531660a33e837643a048c5dfb1b
csvenke <csvenke@users.noreply.github.com>
2024-05-28 21:05:04 +0200

    add dotnet and python devenv

---
+++ python/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Python development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = [
+            (pkgs.python3.withPackages (ps: [ ps.pip ps.pipx ]))
+          ];
+        };
+      }
+    );
+}
