0583371bc8f91b5414008a584f1926116e3e5f5a
csvenke <csvenke@users.noreply.github.com>
2024-06-02 13:56:44 +0200

    feat(fnm-use): create .node-version file with node lts if not exists

--- packages/fnm-use/default.nix
+++ packages/fnm-use/default.nix
@@ -5,7 +5,11 @@ pkgs.mkShell {
     pkgs.fnm
   ];
 
-  shellHook = ''
+  shellHook = /* bash */ ''
+    if [ ! -e .node-version ] && [ ! -e .nvmrc ]; then
+      "${pkgs.nodejs}/bin/node" -v > .node-version
+    fi
+
     eval "$(fnm env --use-on-cd)"
     fnm use --install-if-missing --silent-if-unchanged
   '';
