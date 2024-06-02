e7e4574c0f9b867bffecb66d096f79dabbb4a3fc
csvenke <csvenke@users.noreply.github.com>
2024-06-02 13:52:27 +0200

    ci: add tools flake with release script

---
+++ tools/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Tools";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem = { pkgs, ... }:
+        let
+          inherit (pkgs) callPackage;
+        in
+        {
+          packages.release = callPackage ./release { };
+        };
+    };
+}
