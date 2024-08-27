65bbf5f7cdc48e380df7803b0601546c8945d4b8
csvenke <csvenke@users.noreply.github.com>
2024-08-27 14:53:30 +0200

    feat: update angular devenv

--- flake.nix
+++ flake.nix
@@ -4,9 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    language-servers.url = "git+https://git.sr.ht/~bwolf/language-servers.nix";
   };
 
-  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+  outputs = inputs@{ flake-parts, nixpkgs, language-servers, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       perSystem = { pkgs, ... }:
@@ -26,7 +27,7 @@
             java = callPackage ./packages/java { };
             haskell = callPackage ./packages/haskell { };
             python = callPackage ./packages/python { };
-            angular = callPackage ./packages/angular { };
+            angular = callPackage ./packages/angular { inherit language-servers; };
           };
         };
       flake = {
--- packages/angular/default.nix
+++ packages/angular/default.nix
@@ -1,7 +1,8 @@
-{ pkgs }:
+{ pkgs, language-servers }:
 
 pkgs.mkShell {
   packages = [
     pkgs.nodePackages."@angular/cli"
+    language-servers.packages.angular-language-server
   ];
 }
