d9e8109be2dfb9989e6ecea84676d06fe016ee42
csvenke <csvenke@users.noreply.github.com>
2024-08-27 16:26:51 +0200

    feat(node): update node devenv

--- packages/node/default.nix
+++ packages/node/default.nix
@@ -3,8 +3,16 @@
 pkgs.mkShell {
   packages = [
     pkgs.nodejs
+    pkgs.fnm
     pkgs.bun
     pkgs.yarn
     pkgs.nodePackages.pnpm
   ];
+
+  shellHook = /* bash */ ''
+    if [ -e .node-version ] || [ -e .nvmrc ]; then
+      eval "$(fnm env --use-on-cd)"
+      fnm use --install-if-missing --silent-if-unchanged
+    fi
+  '';
 }
