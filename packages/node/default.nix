c654cfee3b9fb61439c923adc553f9c24aa83420
csvenke <csvenke@users.noreply.github.com>
2024-09-10 16:27:46 +0200

    feat(node)!: remove npm-run alias

--- packages/node/default.nix
+++ packages/node/default.nix
@@ -10,8 +10,6 @@ pkgs.mkShell {
   ];
 
   shellHook = /* bash */ ''
-    alias npm-run="nix run github:csvenke/tools#npm-run"
-
     if [ -e .node-version ] || [ -e .nvmrc ]; then
       eval "$(fnm env --use-on-cd)"
       fnm use --install-if-missing --silent-if-unchanged
